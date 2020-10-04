45c0dd227013fde7f6f5dd9050f08c79c1f009e2
Joshua Arber <arjobosh@gmail.com>
2020-10-03 21:20:25 -0700

    added tracklist show/hide onclick

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-let btn = document.getElementById('btn');
+
+
 let albumsContainer = document.getElementById('albums-container');
-let yearTextbox = document.getElementById('year');
-let year = '';
 
-yearTextbox.addEventListener('input', () => {
-  year = yearTextbox.value;
-});
+let btn = document.getElementById('btn');
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
+  albumsContainer.innerHTML = '';
+  let yearTextbox = document.getElementById('year');
+
   let albumsRequest = new XMLHttpRequest();
 
   // GET request
-  albumsRequest.open('GET', `https://arjobosh.github.io/json/albums/${year}.json`);
-  
+  albumsRequest.open('GET', `https://arjobosh.github.io/json/albums/${yearTextbox.value}.json`);
+
   // once the data is loaded
-  albumsRequest.onload = function() {
+  albumsRequest.onload = function () {
     // defaultly returns as a string, so parse to return as json
     let albumData = JSON.parse(albumsRequest.responseText);
     renderAlbumCard(albumData);
   };
-  
+
   // send the request
   albumsRequest.send();
 });
@@ -32,30 +32,52 @@ function renderAlbumCard(data) {
 
     let cardHeading = document.createElement('span');
     cardHeading.innerHTML = `${album.title} by ${album.artist}`;
+    cardHeading.addEventListener('click', () => {
+      let trackListTable = createTracklistTable(album.trackList);
+      
+      if (!cardHasTable(albumCard, trackListTable)) {
+        albumCard.appendChild(trackListTable);
+      }
+      else {
+        albumCard.lastChild.remove();
+      }
+    });
+
     albumCard.appendChild(cardHeading);
-
-    albumCard.appendChild(createAlbumTable(album.trackList));
     albumsContainer.appendChild(albumCard);
-  } 
+  }
 }
 
-function createAlbumTable(trackList) {
-    let trackListContainer = document.createElement('table');
+function createTracklistTable(trackList) {
+  let trackListContainer = document.createElement('table');
+  trackListContainer.className = 'tracklist-table'
 
-    for (let i = 0; i < trackList.length; i++) {
-      let trackRow = document.createElement('tr');
+  for (let i = 0; i < trackList.length; i++) {
+    let trackRow = document.createElement('tr');
+    trackRow.className = 'track-row';
 
-      trackRow.className = 'track-row';
-      let trackNum = document.createElement('td');
-      trackNum.textContent = `${i+1}.`;
-      trackRow.appendChild(trackNum);
+    let trackNum = document.createElement('td');
+    trackNum.textContent = `${i + 1}.`;
+    trackRow.appendChild(trackNum);
 
-      let trackTitle = document.createElement('td');
-      trackTitle.textContent = `${trackList[i]}`;
-      trackRow.appendChild(trackTitle);
+    let trackTitle = document.createElement('td');
+    trackTitle.textContent = `${trackList[i]}`;
+    trackRow.appendChild(trackTitle);
 
-      trackListContainer.appendChild(trackRow);
+    trackListContainer.appendChild(trackRow);
+  }
+
+  return trackListContainer;
+}
+
+function cardHasTable(card, table) {
+  for (let i = 0; i < card.childNodes.length; i++) {
+    if (card.childNodes[i].className == table.className) {
+      return true;
     }
+  }
 
-    return trackListContainer;
+  return false;
 }
+
+document.body.appendChild(albumsContainer);
